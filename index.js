9008c4166c144382aa969e9979abd191f249dfdf
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-05-09 23:41:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+
+
+app.get("/", (req, res) => {
+  res.send("server is run");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at  http://localhost:${PORT}`);
+});
